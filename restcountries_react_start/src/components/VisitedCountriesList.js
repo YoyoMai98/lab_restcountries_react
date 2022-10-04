07a652b654940ef846d00eb0140eb611d7c7f25d
Yongran <maiyongran@gmail.com>
2022-10-04 16:49:24 +0100

    display a list of visited countries

--- restcountries_react_start/src/components/VisitedCountriesList.js
+++ restcountries_react_start/src/components/VisitedCountriesList.js
@@ -1,11 +1,13 @@
 import CountryListItem from "./CountryListItem";
 
-const VisitedCountriesList = () => {
+const VisitedCountriesList = ({visitedCountries}) => {
     return(
         <>
             <h2>All the countries I've visited!</h2>
-            <CountryListItem/>
-            <CountryListItem/>
+            {visitedCountries ? 
+                visitedCountries.map((country, index) => <CountryListItem country={country.name} key={country+index} />) :
+                <p>Loading...</p>
+            }
         </>
     );
 }
--- restcountries_react_start/src/containers/CountriesContainer.js
+++ restcountries_react_start/src/containers/CountriesContainer.js
@@ -6,6 +6,7 @@ import VisitedCountriesList from "../components/VisitedCountriesList";
 const CountriesContainer = () => {
 
     const [countries, setCountries] = useState([]);
+    const [visitedCountries, setVisitedCountries] = useState([])
 
     const fetchCountries = () => {
         fetch("https://restcountries.com/v3.1/all")
@@ -13,15 +14,35 @@ const CountriesContainer = () => {
         .then(jsonData => setCountries(jsonData))
     }
 
+    const markedVisitedCountries = () => {
+        // const countryIndex = countries.filter(country => {
+        //     return country.name.common.toLowerCase().includes(countryName.toLowerCase())[0]
+        // })
+        
+        // countries[countryIndex].visited = true
+        for(let i = 0; i < 10; i++){
+            countries[i].visited = true
+        }
+        return countries
+    }
+
     useEffect(() => {
         fetchCountries();
-    }, [])
+        for(const country of countries){
+            country.visited = false
+        }
+        setCountries(countries)
+        if(countries.length > 0){
+            setCountries(markedVisitedCountries())
+            setVisitedCountries(countries.filter(country => country.visited))
+        }
+    }, [countries])
 
     return(
         <>
             <h1>I'm a country container!</h1>
             <CountriesList countries={countries} />
-            <VisitedCountriesList/>
+            <VisitedCountriesList visitedCountries={visitedCountries}/>
         </>
     );
 }
